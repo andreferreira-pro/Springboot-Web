5eebd8eb7724669b1a42565cae20bca9a2862621
Andre Ferreira <andre_ferreiraa@hotmail.com>
2025-08-16 11:34:38 -0300

    Associação muito para muitos Product/Category

--- src/main/java/com/educandoweb/course/entities/Product.java
+++ src/main/java/com/educandoweb/course/entities/Product.java
@@ -6,9 +6,13 @@ import java.util.Objects;
 import java.util.Set;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -24,6 +28,8 @@ public class Product implements Serializable {
 	private Double price;
 	private String imgUrl;
 
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
 
 	public Product() {
@@ -79,6 +85,7 @@ public class Product implements Serializable {
 		this.imgUrl = imgUrl;
 	}
 
+	
 	public Set<Category> getCategories() {
 		return categories;
 	}
